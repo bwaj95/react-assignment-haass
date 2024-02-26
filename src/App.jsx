51a9240e80b6d51646bd3f4d24a041fe1123b203
bwaj95 <bwaj95@gmail.com>
2024-02-26 16:06:16 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="fw-flex fw-flex-column">
+      <div className="fw-bg-smoke-25">adasd</div>
+      <div className="fw-bg-smoke-100">asdads</div>
+      <div className="fw-bg-smoke-25">dfgdfg</div>
+      <div className="fw-bg-smoke-100">lksdlflk</div>
+    </div>
+  );
+}
+
+export default App;
